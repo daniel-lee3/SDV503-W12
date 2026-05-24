b1bdc08907510b371b9631bd382e39176129cd1f
daniel-lee3 <daniel-lee3@live.nmit.ac.nz>
2026-05-25 09:43:28 +1200

    Added second question, "How old are you?"

--- index.js
+++ index.js
@@ -7,5 +7,18 @@ const rl = readline.createInterface({
 
 rl.question('What is your name? ', (name) => {
     console.log(`Hello ${name}! Welcome to Node.js readline.`);
-    rl.close();
+
+    rl.question('How old are you? ', (answer) => {
+        const age = Number(answer);
+
+        if (Number.isNaN(age) || age < 0) {
+            console.log(`"${answer}" is not a valid age.`);
+        } else if (age < 18) {
+            console.log(`You are ${age} - still a minor.`);
+        } else {
+            console.log(`You are ${age} - an adult.`);
+        }
+
+        rl.close();
+    });
 });
